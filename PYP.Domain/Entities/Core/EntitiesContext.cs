2210d3aad0b8648f24c5345c2261b73ccc115086
user <user@DESKTOP-LLRNRQK>
2016-08-10 19:10:20 -0400

    Ientityrepository

--- PYP.Domain/Entities/Core/EntitiesContext.cs
+++ PYP.Domain/Entities/Core/EntitiesContext.cs
@@ -16,5 +16,14 @@ namespace PYP.Domain.Entities.Core
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<UserInRole> UserInRoles { get; set; }
+
+        public DbSet<ShipmentType> PackageTypes { get; set; }
+
+        public DbSet<Affiliate> Affiliates { get; set; }
+
+        public DbSet<Shipment> Shipments { get; set; }
+
+        public DbSet<ShipmentState> ShipmentStates { get; set;}
+
     }
 }
