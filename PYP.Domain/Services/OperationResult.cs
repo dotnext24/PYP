89903b21d274a37463e3b3d0b07fe03aa59853e2
user <user@DESKTOP-LLRNRQK>
2016-08-12 20:23:32 -0400

    ImembershipInterface

---
+++ PYP.Domain/Services/OperationResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PYP.Domain.Services
+{
+    public class OperationResult
+    {
+        public bool IsSuccess { get; set;}
+
+        public OperationResult(bool isSuccsess)
+        {
+            IsSuccess = isSuccsess;
+        }
+    }
+}
